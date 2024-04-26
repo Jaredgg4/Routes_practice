d3eac6ba8958460abb55155da90b5733313b75f4
Jaredgg4 <jg70456898@gmail.com>
2024-04-26 15:05:29 -0600

    Adding the code that contains the routes

---
+++ src/pages/Home/Contact.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Contact:React.FC = () =>{
+    return (
+        <p>This is the contact page</p>
+    )
+}
+
+export default Contact;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "@/styles/Home.module.css";
+
+const Message: React.FC = () => {
+    return (
+        <h1 >This is the message</h1>
+    )
+}
+
+export default Message;
